f3d79a92ea57462760226119bf3044c01e90ef9d
nabilahstu4-dev <nabilahstu4@gmail.com>
2026-01-01 01:12:24 +0600

    footer added-nabilah

--- views/Login.php
+++ views/Login.php
@@ -91,4 +91,54 @@ require('../controllers/notifi.php');
 
 <script src="../assets/js/login.js"></script>
 </body>
+<footer>
+    <div class="footer-container">
+        <div class="footer-top">
+            <div class="footer-brand">
+                <h3>FarmLink</h3>
+                <p>Connecting farmers & consumers — Fresh, Fair, Fast.</p>
+                <div class="footer-socials">
+                    <a href="https://facebook.com"><img src="../../assets/img/facebook.png" alt="Facebook"></a>
+                    <a href="https://instagram.com"><img src="../../assets/img/instagram.png" alt="Instagram"></a>
+                    <a href="https://youtube.com"><img src="../../assets/img/youtube.png" alt="YouTube"></a>
+                    <a href="https://twitter.com"><img src="../../assets/img/twitter.png" alt="Twitter"></a>
+                </div>
+            </div>
+            <div class="footer-links">
+                <h4>Quick Links</h4>
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">How It Works</a></li>
+                    <li><a href="#">Marketplace</a></li>
+                    <li><a href="#">Become a Seller</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="footer-support">
+                <h4>Customer Support</h4>
+                <ul>
+                    <li><a href="#">Help Center</a></li>
+                    <li><a href="#">FAQs</a></li>
+                    <li><a href="#">Shipping & Delivery</a></li>
+                    <li><a href="#">Refund Policy</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div class="footer-newsletter">
+                <h4>Stay in the Loop</h4>
+                <p>Get the latest farm-fresh updates & exclusive offers.</p>
+                    <input type="email" placeholder="Enter your email">
+                    <button type="submit">Subscribe</button>
+                <div class="footer-badges">
+                    <button>Fresh & Local</button>
+                    <button>Secure Payments</button>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <span>© 2025 FarmLink. All rights reserved.</span>
+            <span class="footer-made">Made with <span style="color: red;">♥</span> in Bangladesh</span>
+        </div>
+    </div>
+</footer>
 </html>
